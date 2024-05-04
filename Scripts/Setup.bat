2508d643ed00cd1574f7afef132399f06b5c740a
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 07:05:06 +0900

    ci test

--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -4,11 +4,6 @@ pushd "%~dp0"
 rem Initializing submodules
 git submodule update --init --recursive
 
-rem Installing LFS.
-rem A manual install result in not copying the LFS hooks that we already have.
-git lfs install --manual
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 rem Download dependencies.
 ..\UnrealEngine\Engine\DotNET\GitDependencies.exe --no-cache --exclude=Samples --exclude=Templates --exclude=FeaturePacks --exclude=Engine/Documentation --exclude=Engine/Saved --exclude=Win32 --exclude=win32 --exclude=Mac --exclude=osx64 --exclude=osx32 --exclude=Linux --exclude=linux --exclude=Linux32 --exclude=linux32 --exclude=Linux64 --exclude=linux64 --exclude=linux_x64 --exclude=Android --exclude=android --exclude=HoloLens --exclude=Win32 --exclude=WinRT --exclude=winrt --exclude=HTML5 --exclude=html5 --exclude=XboxOne --exclude=Switch --exclude=Dingo --exclude=PS4 --exclude=Python
 if %errorlevel% neq 0 exit /b %errorlevel%
