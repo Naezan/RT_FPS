5d46850f54f8278af4f534a1edab7f1506c1d1b7
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 05:37:56 +0900

    ci test

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,8 +1,8 @@
 cd $PSScriptRoot\..\UnrealEngine
 
 # Build Engine
-. \Setup.bat
-. \GenerateProjectFiles.bat
+Start-Process -FilePath "$PSScriptRoot\..\UnrealEngine\Setup.bat"
+Start-Process -FilePath "$PSScriptRoot\..\UnrealEngine\GenerateProjectFiles.bat"
 # msbuild "$PSScriptRoot\..\RT_FPS.sln" -t:rebuild -verbosity:diag -property:Configuration="Development Editor"
 # . Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=false -set:WithLinuxArm64=false -set:WithClient=true -set:WithServer=true
-. Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
+. Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
