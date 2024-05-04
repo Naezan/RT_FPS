0b0fba7b1b4868f5965e8312817a309640471025
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 08:38:33 +0900

    ci test

--- Scripts/BuildUBT.bat
+++ Scripts/BuildUBT.bat
@@ -3,4 +3,11 @@ pushd "%~dp0"
 
 rem Building UnrealBuildTools
 dotnet msbuild /restore /target:build /property:Configuration=Development ..\UnrealEngine\Engine\Source\Programs\UnrealBuildTool\UnrealBuildTool.csproj
+
+rem Execute BuildTool
+..\UnrealEngine\Engine\Binaries\DotNet\UnrealBuildTool.exe -projectfiles -project="..\RT_FPS.uproject" -game -rocket -progress
+
+rem Building Project
+dotnet msbuild "..\RT_FPS\RT_FPS.sln" /target:build /property:Configuration="Development Editor" /property:Platform=Win64 /verbosity:diagnostic
+
 popd
