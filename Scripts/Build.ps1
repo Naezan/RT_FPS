d48e5275cd3a2b2dc9ccc87c0d102e5c742f41b8
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 08:03:25 +0900

    ci test

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -2,5 +2,6 @@ cd $PSScriptRoot\..\UnrealEngine
 
 # Build Engine
 # msbuild "$PSScriptRoot\..\RT_FPS.sln" -t:rebuild -verbosity:diag -property:Configuration="Development Editor"
-# . Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=false -set:WithLinuxArm64=false -set:WithClient=true -set:WithServer=true
+# ..\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script="%~dp0\..\UnrealEngine\Engine\Build\InstalledEngineBuild.xml" -set:CompileDatasmithPlugins=false -set:HostPlatformOnly=true -set:GameConfigurations=Development -set:WithDDC=false
+. Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script="$PSScriptRoot\..\UnrealEngine\Engine\Build\InstalledEngineBuild.xml" -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=false -set:WithLinuxArm64=false -set:WithClient=true -set:WithServer=true
 . Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -targetplatform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -skipcookingeditorcontent -unversionedcookedcontent -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
--- Scripts/BuildUBT.bat
+++ Scripts/BuildUBT.bat
@@ -3,5 +3,4 @@ pushd "%~dp0"
 
 rem Building UnrealBuildTools
 dotnet msbuild /restore /target:build /property:Configuration=Development ..\UnrealEngine\Engine\Source\Programs\UnrealBuildTool\UnrealBuildTool.csproj
-..\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script="%~dp0\..\UnrealEngine\Engine\Build\InstalledEngineBuild.xml" -set:CompileDatasmithPlugins=false -set:HostPlatformOnly=true -set:GameConfigurations=Development -set:WithDDC=false
 popd
