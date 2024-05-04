820c8db2728f04b08e04f39ec68c337ab0e0a778
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 03:46:22 +0900

    ci test

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,3 +1,5 @@
 cd $PSScriptRoot\..\UnrealEngine
 
-. Engine\Build\BatchFiles\Build.bat RT_FPSEditor Win64 Development -Project="$PSScriptRoot\..\RT_FPS.uproject" -waitmutex -NoHotReload
+# Build Engine
+# . \Engine\Build\BatchFiles\RunUAT.bat BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=false -set:WithLinuxArm64=false -set:WithClient=true -set:WithServer=true
+. \Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="$PSScriptRoot\..\Build"
