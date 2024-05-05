b6e17a952fe1bffd9171844e695fff0048881f63
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 09:44:09 +0900

    ci test

---
+++ Scripts/Package.ps1
@@ -0,0 +1,5 @@
+cd $PSScriptRoot\..\UnrealEngine
+
+# Build Engine
+. Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -maps=AllMaps -compile -stage -pak -archive -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
+# . Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -targetplatform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -skipcookingeditorcontent -unversionedcookedcontent -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
