8274d69abe1a4e9d4a56bdcac593325bb5fde43e
Naezan <64336496+Naezan@users.noreply.github.com>
2024-05-05 09:17:06 +0900

    ci test

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -1,10 +1,4 @@
 cd $PSScriptRoot\..\UnrealEngine
 
 # Build Engine
-. Engine\Binaries\DotNet\UnrealBuildTool.exe -projectfiles -project="$PSScriptRoot\..\RT_FPS.uproject" -game -rocket -progress
-
-$batCommand = ".\dotnet msbuild ""$PSScriptRoot\..\RT_FPS.sln"" /target:build /property:Configuration=""Development Editor"" /property:Platform=Win64 /verbosity:diagnostic"
-Invoke-Expression -Command $batCommand
-# dotnet msbuild "$PSScriptRoot\..\RT_FPS.sln" /target:build /property:Configuration="Development Editor" /property:Platform=Win64 /verbosity:diagnostic
-
 . Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="$PSScriptRoot\..\RT_FPS.uproject" -noP4 -platform=Win64 -targetplatform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -skipcookingeditorcontent -unversionedcookedcontent -archivedirectory="$PSScriptRoot\..\Build\RT_FPS"
--- Scripts/BuildUBT.bat
+++ Scripts/BuildUBT.bat
@@ -4,4 +4,10 @@ pushd "%~dp0"
 rem Building UnrealBuildTools
 dotnet msbuild /restore /target:build /property:Configuration=Development ..\UnrealEngine\Engine\Source\Programs\UnrealBuildTool\UnrealBuildTool.csproj
 
+rem Execute BuildTool
+D:\a\RT_FPS\RT_FPS\UnrealEngine\Engine\Binaries\DotNET\UnrealBuildTool.exe -projectfiles -project="D:\a\RT_FPS\RT_FPS\RT_FPS.uproject" -game -rocket -progress
+
+rem Building Project
+dotnet msbuild "D:\a\RT_FPS\RT_FPS\RT_FPS\RT_FPS.sln" /target:build /property:Configuration="Development Editor" /property:Platform=Win64 /verbosity:diagnostic
+
 popd
